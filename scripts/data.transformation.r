ee42e4af1cbfcad00567e5c906b253a2ff278df8
alexei-kouprianov <alexei.kouprianov@gmail.com>
2021-06-17 17:55:15 +0300

    Data (partially) updated to 2021-06-17

--- scripts/data.transformation.r
+++ scripts/data.transformation.r
@@ -1,3 +1,6 @@
+# Loading data
 spb.lcb_primary <- read.table("../data/primary/covid.SPb.gov.spb.ru.overview.txt", h=TRUE, sep="\t")
 spb.sk_primary <- read.table("../data/primary/covid.SPb.stopkoronavirus.rf.txt", h=TRUE, sep="\t")
+spb.h_primary <- read.table("../data/primary/covid.SPb.gov.spb.ru.hospitalization.txt", h=TRUE, sep="\t")
+spb.PCR_primary <- read.table("../data/primary/covid.SPb.PCRtests.txt", h=TRUE, sep="\t")
 
