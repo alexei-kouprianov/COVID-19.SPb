8a9f0a22fe7fc3e3f13e6ce76831dc4b6eb947f9
alexei-kouprianov <alexei.kouprianov@gmail.com>
2021-06-17 17:58:53 +0300

    Bug fixed

--- private/scripts/copy_primary_files.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cp /home/tinea/Documents/H_et_S/Projects/github/COVID.2019.ru/private/data/spb.zaks/spb.zaks.trimmed.txt /home/tinea/Documents/H_et_S/Projects/github/COVID-19.SPb/data/primary/covid.SPb.gov.spb.ru.overview.txt
-cp /home/tinea/Documents/H_et_S/Projects/github/COVID.2019.ru/data/covid.2019.ru.dyn.tot.primary.SPb.txt /home/tinea/Documents/H_et_S/Projects/github/COVID-19.SPb/data/primary/covid.SPb.stopkoronavirus.rf.txt
-cp /home/tinea/Documents/H_et_S/Projects/github/COVID.2019.ru/private/data/spb.zaks/spb.hospitalized_today.txt /home/tinea/Documents/H_et_S/Projects/github/COVID-19.SPb/data/primary/covid.SPb.gov.spb.ru.hospitalization.txt
-Rscript /home/tinea/Documents/H_et_S/Projects/github/COVID-19.SPb/private/scripts/telegram.trimmer.r
-
