225ac56845f266ec5beb14c9d3f77011c871e95b
alexei-kouprianov <alexei.kouprianov@gmail.com>
2021-06-27 17:48:37 +0300

    Yandex activity primary added; Variables to summary tables added;

--- scripts/data.transformation.r
+++ scripts/data.transformation.r
@@ -4,6 +4,7 @@ spb.lcb_primary <- read.table("../data/primary/covid.SPb.gov.spb.ru.overview.txt
 spb.sk_primary <- read.table("../data/primary/covid.SPb.stopkoronavirus.rf.txt", h=TRUE, sep="\t")
 spb.h_primary <- read.table("../data/primary/covid.SPb.gov.spb.ru.hospitalization.txt", h=TRUE, sep="\t")
 spb.PCR_primary <- read.table("../data/primary/covid.SPb.PCRtests.txt", h=TRUE, sep="\t")
+spb.yandex_activity_primary <- read.table("../data/primary/covid.SPb.yandex.activity.points.daily.txt", h=TRUE, sep="\t")
 
 ################################################################
 # Trimming dates;
@@ -11,6 +12,7 @@ spb.lcb_primary$TIME <- strptime(spb.lcb_primary$DATE, "%Y-%m-%d")
 spb.sk_primary$TIME <- strptime(spb.sk_primary$TIME, "%Y-%m-%d")
 spb.h_primary$TIME <- strptime(spb.h_primary$DATE, "%Y-%m-%d")
 spb.PCR_primary$TIME <- strptime(spb.PCR_primary$DATE, "%Y-%m-%d")
+spb.yandex_activity_primary$TIME <- strptime(spb.yandex_activity_primary$DATE, "%Y-%m-%d")
 
 ################################################################
 # Building a combined data frame;
@@ -23,15 +25,20 @@ spb.h_combined <- cbind.data.frame(
 	spb.lcb_primary$TIME[173:176])
 	)
 colnames(spb.h_combined) <- c("HOSPITALIZED.today", "TIME")
+
 spb.PCR_truncated <- spb.PCR_primary[1:423, 2:3]
+
+spb.yandex_activity_truncated <- spb.yandex_activity_primary[15:471,]
 
 spb.combined <- cbind.data.frame(
 	spb.sk_truncated,
+	c(rep(NA, (nrow(spb.sk_truncated)-nrow(spb.lcb_primary))), spb.lcb_primary$i),
+	spb.h_combined$HOSPITALIZED.today,
 	c(rep(NA, (nrow(spb.sk_truncated)-nrow(spb.PCR_truncated))), spb.PCR_truncated$TESTS),
-	spb.h_combined$HOSPITALIZED.today
+	spb.yandex_activity_truncated$Yandex.ACTIVITY.points
 	)
 
-colnames(spb.combined) <- c("TIME", "CONFIRMED", "RECOVERED", "DEATHS", "ACTIVE", "PCR.tested", "HOSPITALIZED.today")
+colnames(spb.combined) <- c("TIME", "CONFIRMED", "RECOVERED", "DEATHS", "ACTIVE", "CONFIRMED.spb", "HOSPITALIZED.today", "PCR.tested", "Yandex.ACTIVITY.points")
 
 ################################################################
 # Building a combined data frame aggregated by weeks;
@@ -41,8 +48,10 @@ weeks.CONFIRMED <- NULL
 weeks.RECOVERED <- NULL
 weeks.DEATHS <- NULL
 weeks.ACTIVE <- NULL
-weeks.PCR.tested <- NULL
+weeks.CONFIRMED.spb <- NULL
 weeks.HOSPITALIZED.today <- NULL
+weeks.PCR.tested <- NULL
+weeks.Yandex.ACTIVITY.points <- NULL
 
 for(i in 1:(nrow(spb.combined) %/% 7)){
 	weeks.TIME <- c(weeks.TIME, as.character(spb.combined$TIME)[i*7])
@@ -50,19 +59,23 @@ for(i in 1:(nrow(spb.combined) %/% 7)){
 	weeks.RECOVERED <- c(weeks.RECOVERED, sum(spb.combined$RECOVERED[(1+(i-1)*7):(7+(i-1)*7)], na.rm=TRUE))
 	weeks.DEATHS <- c(weeks.DEATHS, sum(spb.combined$DEATHS[(1+(i-1)*7):(7+(i-1)*7)], na.rm=TRUE))
 	weeks.ACTIVE <- c(weeks.ACTIVE, spb.combined$ACTIVE[i*7])
-	weeks.PCR.tested <- c(weeks.PCR.tested, sum(spb.combined$PCR.tested[(1+(i-1)*7):(7+(i-1)*7)], na.rm=TRUE))
+	weeks.CONFIRMED.spb <- c(weeks.CONFIRMED.spb, sum(spb.combined$CONFIRMED.spb[(1+(i-1)*7):(7+(i-1)*7)], na.rm=TRUE))
 	weeks.HOSPITALIZED.today <- c(weeks.HOSPITALIZED.today, sum(spb.combined$HOSPITALIZED.today[(1+(i-1)*7):(7+(i-1)*7)], na.rm=TRUE))
+	weeks.PCR.tested <- c(weeks.PCR.tested, sum(spb.combined$PCR.tested[(1+(i-1)*7):(7+(i-1)*7)], na.rm=TRUE))
+	weeks.Yandex.ACTIVITY.points <- c(weeks.Yandex.ACTIVITY.points, mean(spb.combined$Yandex.ACTIVITY.points[(1+(i-1)*7):(7+(i-1)*7)], na.rm=TRUE))
 }
 
 spb.combined.weeks <- cbind.data.frame(
-weeks.TIME,
-weeks.CONFIRMED,
-weeks.RECOVERED,
-weeks.DEATHS,
-weeks.ACTIVE,
-weeks.PCR.tested,
-weeks.HOSPITALIZED.today
-)
+	weeks.TIME,
+	weeks.CONFIRMED,
+	weeks.RECOVERED,
+	weeks.DEATHS,
+	weeks.ACTIVE,
+	weeks.CONFIRMED.spb,
+	weeks.HOSPITALIZED.today,
+	weeks.PCR.tested,
+	weeks.Yandex.ACTIVITY.points
+	)
 
 colnames(spb.combined.weeks) <- colnames(spb.combined)
 
